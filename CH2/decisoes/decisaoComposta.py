cc245430ecfd11756a602403ad02c805a540a80d
XandP <alexandre.rochap@hotmail.com>
2021-12-29 19:15:45 -0300

    Criados programas para exemplificar decisão encadeada no Python.

--- CH2/decisoes/decisaoComposta.py
+++ CH2/decisoes/decisaoComposta.py
@@ -1,9 +1,12 @@
-nome = input("Digite o nome: ")
 idade = int(input("Digite a idade: "))
-doenca_infectiocontagiosa=input("Suspeita de doença infectio-contagiosa?").upper()
-if idade >= 65:
-    print("O paciente " + nome + " POSSUI atendimento prioritário!")
-elif doenca_infectiocontagiosa == "SIM":
-    print("O paciente " + nome + " deve ser direcionado para sala de espera reservada.")
+doenca_infectocontagiosa = input("Suspeita de doença infecto-contagiosa? ").upper()
+if idade >= 65 and doenca_infectocontagiosa == "SIM":
+    print("O paciente será direcionado para sala AMARELA - COM prioridade")
+elif idade < 65 and doenca_infectocontagiosa == "SIM":
+    print("O paciente será direcionado para sala AMARELA - SEM prioridade")
+elif idade >= 65 and doenca_infectocontagiosa == "NAO":
+    print("O paciente será direcionado para sala BRANCA - COM prioridade")
+elif idade < 65 and doenca_infectocontagiosa == "NAO":
+    print("O paciente será direcionado para sala BRANCA - SEM prioridade")
 else:
-    print("O paciente " + nome + " NÃO possui atendimento prioritário e pode aguardar na sala comum!")
+    print("Responda a suspeita de doença infectocontagiosa com SIM ou NAO")
